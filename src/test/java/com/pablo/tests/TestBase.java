2e2873bb5ef92284d1a46dd0e8da00b8be3ec89d
PoulPerv <dizel_tdi@bk.ru>
2022-05-27 20:25:22 +0300

    add browser

--- src/test/java/com/pablo/tests/TestBase.java
+++ src/test/java/com/pablo/tests/TestBase.java
@@ -16,7 +16,7 @@ public class TestBase {
     static void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
-        Configuration.browser = "chrome";
+        Configuration.browser = System.getProperty("browser");
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
